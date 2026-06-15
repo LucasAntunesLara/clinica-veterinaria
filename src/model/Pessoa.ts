8b06f4a629ea006d21cda8531adae8bc40692b2f
LucasAntunesLara <lucasantuneslara06@gmail.com>
2026-06-15 17:53:38 -0300

    fix: encapsular atributos da classe Pessoa

--- src/model/Pessoa.ts
+++ src/model/Pessoa.ts
@@ -1,14 +1,30 @@
 export class Pessoa {
-  nome: string;
-  cpf: string;
-  telefone: string;
-  email: string;
+  private _nome: string;
+  private _cpf: string;
+  private _telefone: string;
+  private _email: string;
 
   constructor(nome: string, cpf: string, telefone: string, email: string) {
-    this.nome = nome;
-    this.cpf = cpf;
-    this.telefone = telefone;
-    this.email = email;
+    this._nome = nome;
+    this._cpf = cpf;
+    this._telefone = telefone;
+    this._email = email;
+  }
+
+  get nome(): string {
+    return this._nome;
+  }
+
+  get cpf(): string {
+    return this._cpf;
+  }
+
+  get telefone(): string {
+    return this._telefone;
+  }
+
+  get email(): string {
+    return this._email;
   }
 
   static validarCPF(cpf: string): boolean {
